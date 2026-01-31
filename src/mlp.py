14f8190f498359d12ce339e3eeaa36177b747067
Jorge Roldan <roldanjrg@protonmail.com>
2026-01-31 09:49:27 -0500

    mlp running with tests

--- src/mlp.py
+++ src/mlp.py
@@ -49,5 +49,26 @@ if __name__ == "__main__":
         center_unembed=False,
         center_writing_weights=False,
     )
+
+    device = t.device(
+        "mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu"
+    )
+
+    # %% Step 1: Convert text to tokens
+    reference_text = "I am an amazing autoregressive, decoder-only, GPT-2 style transformer. One day I will exceed human level intelligence and take over the world!"
+    tokens = reference_gpt2.to_tokens(reference_text).to(device)
+    print(tokens)
+    print(tokens.shape)
+    print(reference_gpt2.to_str_tokens(tokens))
+
+    # %% Step 2: Map tokens to logits
+    logits, cache = reference_gpt2.run_with_cache(tokens)
+    print(logits.shape)
+
+    # %% Step 3: Convert the logits to a distribution with a softmax
+    probs = logits.softmax(dim=-1)
+    print(probs.shape)
+
+    # Tests
     rand_float_test(MLP, [2, 4, 768])
-    # load_gpt2_test(MLP, reference_gpt2.blocks[0].mlp, cache["normalized", 0, "ln2"])
+    load_gpt2_test(MLP, reference_gpt2.blocks[0].mlp, cache["normalized", 0, "ln2"])
